e31d0f760bb8359aa25bea4647d2086b01d11123
jigenanto <jigenanto@proton.me>
2026-05-14 22:19:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,27 +3,19 @@
 cd /app
 
 echo "Registering WARP..."
-
 yes | ./wgcf register
 
 echo "Generating profile..."
-
 ./wgcf generate
+
+echo "Adding HTTP proxy config..."
+
+cat >> wgcf-profile.conf <<EOF
+
+[http]
+BindAddress = 0.0.0.0:8888
+EOF
 
 echo "Starting wireproxy..."
 
-./wireproxy --config wgcf-profile.conf &
-
-sleep 5
-
-echo "Creating tinyproxy config..."
-
-cat > /tmp/tinyproxy.conf <<EOF
-Port 8888
-Timeout 600
-LogLevel Info
-EOF
-
-echo "Starting tinyproxy..."
-
-tinyproxy -d -c /tmp/tinyproxy.conf
+./wireproxy --config wgcf-profile.conf
