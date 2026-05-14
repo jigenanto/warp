3a89920ffc04d011be27270cb56ae40ee59c3a74
jigenanto <jigenanto@proton.me>
2026-05-14 18:26:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,40 +10,12 @@ echo "Generating profile..."
 
 ./wgcf generate
 
-PRIVATE_KEY=$(grep PrivateKey wgcf-profile.conf | awk '{print $3}')
-PUBLIC_KEY=$(grep PublicKey wgcf-profile.conf | awk '{print $3}')
-
-cat > warp.json <<EOF
-{
-  "bind_address": "127.0.0.1:1080",
-  "workers": 1,
-  "local_dns": false,
-  "ipv6": false,
-  "peers": [
-    {
-      "public_key": "$PUBLIC_KEY",
-      "endpoint": "engage.cloudflareclient.com:2408",
-      "keepalive": 25,
-      "allowed_ips": [
-        "0.0.0.0/0"
-      ]
-    }
-  ],
-  "interface": {
-    "private_key": "$PRIVATE_KEY",
-    "addresses": [
-      "172.16.0.2/32"
-    ]
-  }
-}
-EOF
-
 echo "Starting wireproxy..."
 
-./wireproxy -c warp.json &
+./wireproxy --config wgcf-profile.conf &
 
 sleep 5
 
 echo "Starting tinyproxy..."
 
-tinyproxy -d
+tinyproxy -d -c /etc/tinyproxy/tinyproxy.conf
