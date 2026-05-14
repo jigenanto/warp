f4359321842e1396ef92b0aa6a294438619794c7
jigenanto <jigenanto@proton.me>
2026-05-14 17:55:50 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,57 @@
 #!/bin/sh
 
-echo "Starting WARP..."
-/app/wireproxy -c /app/warp.json &
+cd /app
+
+echo "Registering WARP..."
+
+yes | ./wgcf register
+
+echo "Generating profile..."
+
+./wgcf generate
+
+cat > warp.conf <<EOF
+[Interface]
+PrivateKey = $(grep PrivateKey wgcf-profile.conf | cut -d ' ' -f3)
+Address = 172.16.0.2/32
+
+[Peer]
+PublicKey = $(grep PublicKey wgcf-profile.conf | cut -d ' ' -f3)
+AllowedIPs = 0.0.0.0/0
+Endpoint = engage.cloudflareclient.com:2408
+EOF
+
+cat > warp.json <<EOF
+{
+  "bind_address": "127.0.0.1:1080",
+  "workers": 1,
+  "local_dns": false,
+  "ipv6": false,
+  "peers": [
+    {
+      "public_key": "$(grep PublicKey wgcf-profile.conf | cut -d ' ' -f3)",
+      "endpoint": "engage.cloudflareclient.com:2408",
+      "keepalive": 25,
+      "allowed_ips": [
+        "0.0.0.0/0"
+      ]
+    }
+  ],
+  "interface": {
+    "private_key": "$(grep PrivateKey wgcf-profile.conf | cut -d ' ' -f3)",
+    "addresses": [
+      "172.16.0.2/32"
+    ]
+  }
+}
+EOF
+
+echo "Starting wireproxy..."
+
+./wireproxy -c warp.json &
 
 sleep 5
 
-echo "Starting TinyProxy..."
+echo "Starting tinyproxy..."
+
 tinyproxy -d
